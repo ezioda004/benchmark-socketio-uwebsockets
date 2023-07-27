b1406ee71405275751a60cce898b71f51634a58d
rajat.malhotra <rajat.malhotra@vedantu.com>
2023-07-27 10:38:41 +0530

    manual cluster

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,31 @@
 import { UWebSockets } from "./uwebsockets/uwebsockets.js";
 import { redis } from "./redis.js"
 
-// import cluster from 'node:cluster';
-// import { availableParallelism } from 'node:os';
-// import process from 'node:process';
+import cluster from 'node:cluster';
+import { availableParallelism } from 'node:os';
+import process from 'node:process';
 
-// const numCPUs = availableParallelism();
+const numCPUs = availableParallelism();
 
-// if (cluster.isPrimary) {
-//   console.log(`Primary ${process.pid} is running`);
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
 
-//   cleanupRedis();
-//   // Fork workers.
-//   for (let i = 0; i < numCPUs / 2; i++) {
-//     cluster.fork();
-//   }
+  cleanupRedis();
+  // Fork workers.
+  for (let i = 0; i < numCPUs / 2; i++) {
+    cluster.fork();
+  }
 
-//   cluster.on('exit', (worker, code, signal) => {
-//     console.log(`worker ${worker.process.pid} died`);
-//   });
-// } else {
-//   // Workers can share any TCP connection
-//   // In this case it is an HTTP server
-//   main();
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  main();
 
-//   console.log(`Worker ${process.pid} started`);
-// }
+  console.log(`Worker ${process.pid} started`);
+}
 
 async function startUWebSockets() {
   const uwebsockets = new UWebSockets();
@@ -41,4 +41,4 @@ async function main() {
   await startUWebSockets();
 }
 
-main();
+// main();
