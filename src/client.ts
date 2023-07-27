72373148567324006670307523c53df9839a0d62
rajat.malhotra <rajat.malhotra@vedantu.com>
2023-07-27 19:39:46 +0530

    client cluster

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,30 @@
 import { WebSocketClient } from "./uwebsockets/websocketClient.js";
 import { SocketClient } from "./socketio/socketioClient.js"
 
+import cluster from 'node:cluster';
+import { availableParallelism } from 'node:os';
+import process from 'node:process';
+
+const numCPUs = availableParallelism();
+
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs - 1; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  main();
+
+  console.log(`Worker ${process.pid} started`);
+}
 
 function main() {
     console.log("main");
@@ -28,4 +52,4 @@ function main() {
     }
 }
 
-main();
+// main();
