eab9aa3ad4da230701212902e726c5d06128eebd
rajat.malhotra <rajat.malhotra@vedantu.com>
2023-08-01 17:01:40 +0530

    fixes

--- httpserver/hyper.js
+++ httpserver/hyper.js
@@ -1,12 +1,19 @@
 import HyperExpress from "hyper-express";
 const webserver = new HyperExpress.Server();
+const PORT = 3000;
 
 // Create GET route to serve 'Hello World'
-webserver.get('/', (request, response) => {
+webserver
+.get('/', (request, response) => {
+    response.send(Buffer.alloc(2048, 'hello world').toString());
+})
+
+webserver
+.get('/hello', (request, response) => {
     response.send(Buffer.alloc(2048, 'hello world').toString());
 })
 
 // Activate webserver by calling .listen(port, callback);
-webserver.listen(9001)
-.then((socket) => console.log('Webserver started on port 80'))
-.catch((error) => console.log('Failed to start webserver on port 80'));
+webserver.listen(PORT)
+.then((socket) => console.log(`Webserver started on port ${PORT}`))
+.catch((error) => console.log(`Failed to start webserver on port ${PORT}`));
--- httpserver/uwebsockets.js
+++ httpserver/uwebsockets.js
@@ -1,11 +1,19 @@
 import { App } from "uwebsockets.js";
+const PORT = 3000;
 
-App().get('/hello', (res, req) => {
+App()
+.get('/', (res, req) => {
+  res.writeStatus('200 OK').writeHeader('IsExample', 'Yes')
+  .end('hello world');
+})
+.get('/hello', (res, req) => {
 
   /* It does Http as well */
-  res.writeStatus('200 OK').writeHeader('IsExample', 'Yes').end(Buffer.alloc(2048, 'hello world').toString());
+  res.writeStatus('200 OK').writeHeader('IsExample', 'Yes')
+  .end('hello world');
+  // .end(Buffer.alloc(2048, 'hello world').toString());
   
-}).listen(9001, (listenSocket) => {
+}).listen(PORT, (listenSocket) => {
 
   if (listenSocket) {
     console.log('Listening to port 9001');
